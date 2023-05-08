2966770a3cb5ca66c4bf98a42ba633427d73de61
Akash <124170332+Akashpandey1507@users.noreply.github.com>
2023-05-08 09:38:53 +0530

    Add files via upload

---
+++ Triange_Pattern.py
@@ -0,0 +1,9 @@
+for i in range(10,0,-1):
+    for j in range(10,i,-1):
+        print('*',end='   ')
+    print()
+
+for i in range(10):
+    for j in range(i,10):
+        print('*',end='   ')
+    print()
