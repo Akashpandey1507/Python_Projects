69755c9c6664ce9362937b3d35d45d46db2472b6
Akash <124170332+Akashpandey1507@users.noreply.github.com>
2023-05-19 14:14:06 +0530

    Add files via upload

--- All in one.py
+++ All in one.py
@@ -9,6 +9,8 @@ choose = input('''Please press -
 5 For BMI Calculator
 6 For Even Number
 7 For Sentence Charactor Count
+8 For Calculate the Time Value of money for one time investment
+
 
 Choose::: ''')
 
@@ -105,3 +107,24 @@ elif choose == '7':
     for Char,Counts in count.items():
         print(f'{Char} : {Counts}')
 
+elif choose =='8':
+    
+    # Calculate the Time Value of money for one time investment
+
+    Amount = float(input('Enter your Investment Amount: '))
+    Interest_rate = float(input('Enter your Rate of Interest Rate: '))
+    Time = float(input('Enter your Time in Years: '))
+
+    Present_Value = Amount
+    Rate = Interest_rate / 100
+    Time = Time
+
+    print(f'''Question: If a Person invests Rs. {Present_Value} in an investment which pays {Interest_rate}% of interest, Wthat will be the Future Value of the invested value amount at the end of {Time} years.''')
+
+
+    Future_Value = round(Present_Value * (1 + Rate) ** Time ,1)
+
+    print(f'The Future Value will be: Rs. {Future_Value}')
+
+
+        
