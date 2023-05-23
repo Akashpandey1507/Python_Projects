3db0ebc6e3f38ce22fa9eb03ae949285893ec3b4
Akash <124170332+Akashpandey1507@users.noreply.github.com>
2023-05-23 13:54:27 +0530

    Delete Lamba Sqaure & Cube.py

--- Lamba Sqaure & Cube.py
+++
@@ -1,7 +0,0 @@
-
-Sqaure = lambda x : x**2
-Cube = lambda x : x**3
-
-number = int(input('Enter the Number for Cube and Sqr : '))
-print(f"The Square is: {Sqaure(number)}")
-print(f"The Cube is: {Cube(number)}")
